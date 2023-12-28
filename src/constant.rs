c1ebec8c098ea925920a20145146c37771eb9b1d
Kong <tjbuggo@gmail.com>
2023-12-28 18:27:43 +0800

    add sun

--- src/constant.rs
+++ src/constant.rs
@@ -6,8 +6,10 @@ pub mod Earth {
     use super::*;
     use std::f32::consts::PI;
 
+    pub const ORBITAL_VELCITY: f32 = 2.0 * PI / (365.0 * ONE_DAY_SECONDS as f32);
     pub const ROTATION_VELCITY: f32 = 2.0 * PI / ONE_DAY_SECONDS as f32;
     pub const RADIUS: f32 = 6371.0;
+    pub const DISTANCE_TO_SUN: f32 = 149_597_871.0;
 }
 
 pub mod Moon {
@@ -15,7 +17,7 @@ pub mod Moon {
     use std::f32::consts::PI;
     const ORBITAL_PERIOD: f32 = 27.3; // day
     pub const ORBITAL_VELCITY: f32 = 2.0 * PI / (ORBITAL_PERIOD * ONE_DAY_SECONDS as f32);
-    pub const ROTATION_VELCITY: f32 = ORBITAL_VELCITY;
+    pub const ROTATION_VELCITY: f32 = 2.0 * PI / ONE_DAY_SECONDS as f32;
     pub const RADIUS: f32 = 1737.4;
     pub const DISTANCE_TO_EARTH: f32 = 384400.0;
 }
--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,11 @@ use bevy::{
 use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
 
 use button::{button_handler, mouse_button_input, scroll_events, touchpad_gestures};
+use constant::{Earth::DISTANCE_TO_SUN, Moon::DISTANCE_TO_EARTH, SPACE_SCALE, TIME_SCALE};
 #[derive(Component)]
-struct Earth;
+struct Earth {
+    distance_to_sun: f32,
+}
 
 #[derive(Component)]
 struct Moon {
@@ -32,6 +35,8 @@ enum CameraFocusType {
     Moon,
     Global,
 }
+
+const distance_to_sun: f32 = 200.0;
 
 fn main() {
     App::new()
@@ -62,7 +67,12 @@ fn main() {
         .run();
 }
 
-fn move_moon(time: Res<Time>, mut query: Query<(&mut Transform, &Moon)>) {
+fn move_moon(
+    time: Res<Time>,
+    mut query: Query<(&mut Transform, &Moon)>,
+    earth_query: Query<(&Transform, &Earth), Without<Moon>>,
+) {
+    let earth = earth_query.single();
     for (mut transform, moon) in &mut query {
         // let movement = transf.forward() + transf.left();
         transform.rotate_y(
@@ -71,18 +81,19 @@ fn move_moon(time: Res<Time>, mut query: Query<(&mut Transform, &Moon)>) {
 
         let elapsed_seconds = time.elapsed_seconds();
         let angle = elapsed_seconds * constant::TIME_SCALE * constant::Moon::ORBITAL_VELCITY;
-        transform.translation = Vec3 {
-            x: moon.distance_to_earth * angle.cos(),
-            y: 0.0,
-            z: moon.distance_to_earth * angle.sin(),
-        };
+        transform.translation = earth.0.translation
+            + Vec3 {
+                x: moon.distance_to_earth * angle.cos(),
+                y: 0.0,
+                z: moon.distance_to_earth * angle.sin(),
+            };
     }
 }
 
-fn move_earth(time: Res<Time>, mut query: Query<&mut Transform, With<Earth>>) {
-    let mut transf = query.single_mut();
-    transf
-        .rotate_y(constant::TIME_SCALE * constant::Earth::ROTATION_VELCITY * time.delta_seconds());
+fn move_earth(time: Res<Time>, mut query: Query<(&mut Transform, &Earth)>) {
+    // let mut transf = query.single_mut();
+    // transf
+    //     .rotate_y(constant::TIME_SCALE * constant::Earth::ROTATION_VELCITY * time.delta_seconds());
 
     // let elapsed_seconds = time.elapsed_seconds();
     // let angle = elapsed_seconds * constant::Moon::VELCITY;
@@ -91,6 +102,19 @@ fn move_earth(time: Res<Time>, mut query: Query<&mut Transform, With<Earth>>) {
     //     y: 0.0,
     //     z: 25.0 * angle.sin(),
     // };
+
+    for (mut transform, earth) in &mut query {
+        // let movement = transf.forward() + transf.left();
+        transform.rotate_y(TIME_SCALE * constant::Earth::ROTATION_VELCITY * time.delta_seconds());
+
+        let elapsed_seconds = time.elapsed_seconds();
+        let angle = elapsed_seconds * TIME_SCALE * constant::Earth::ORBITAL_VELCITY;
+        transform.translation = Vec3 {
+            x: earth.distance_to_sun * angle.cos(),
+            y: 0.0,
+            z: earth.distance_to_sun * angle.sin(),
+        };
+    }
 }
 
 fn setup(
@@ -99,28 +123,47 @@ fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
     asset_server: Res<AssetServer>,
 ) {
-    // commands.spawn(PointLightBundle {
-    //     point_light: PointLight {
-    //         intensity: 9000.0,
-    //         range: 100.,
-    //         shadows_enabled: true,
-    //         ..default()
-    //     },
-    //     transform: Transform::from_xyz(8.0, 16.0, 8.0),
+    commands
+        .spawn(PointLightBundle {
+            point_light: PointLight {
+                intensity: 9000.0,
+                // range: 300.,
+                // radius: 60.0,
+                shadows_enabled: true,
+                ..default()
+            },
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
+            ..default()
+        })
+        .with_children(|builder| {
+            let scene = asset_server.load("sun.glb#Scene0");
+            builder.spawn((SceneBundle {
+                scene: scene,
+                transform: Transform::from_xyz(0., 0., 0.).with_scale(Vec3::splat(50.)),
+                ..default()
+            },));
+        });
+
+    // Sun
+    // let scene = asset_server.load("sun.glb#Scene0");
+    // commands.spawn((SceneBundle {
+    //     scene: scene,
+    //     transform: Transform::from_xyz(0., 0., 0.).with_scale(Vec3::splat(50.)),
     //     ..default()
-    // });
+    // },));
 
     // Earth
-    let es = asset_server.load("earth.glb#Scene0");
-    println!("{:?}", es);
+    let es = asset_server.load("earth2.glb#Scene0");
     commands.spawn((
         SceneBundle {
             scene: es,
-            transform: Transform::from_xyz(0., 0., 0.),
-            // .with_scale(Vec3::splat(constant::SPACE_SCALE)),
+            transform: Transform::from_xyz(distance_to_sun, 0., 0.)
+                .with_scale(Vec3::splat(1.0 / 100.)),
             ..default()
         },
-        Earth,
+        Earth {
+            distance_to_sun: distance_to_sun,
+        },
     ));
     // .with_children(|parent| {
     //     // child cube
@@ -162,11 +205,7 @@ fn setup(
         // },
         SceneBundle {
             scene: asset_server.load("moon.glb#Scene0"),
-            transform: Transform::from_xyz(
-                constant::Moon::DISTANCE_TO_EARTH * constant::SPACE_SCALE,
-                0.,
-                0.,
-            ),
+            transform: Transform::from_xyz(100.0 + DISTANCE_TO_EARTH * SPACE_SCALE, 0., 0.),
             // .with_scale(Vec3::splat(0.1)),
             ..default()
         },
@@ -189,13 +228,19 @@ fn setup(
     ));
 }
 
-fn axis(mut gizmos: Gizmos, time: Res<Time>) {
+fn axis(mut gizmos: Gizmos, time: Res<Time>, query: Query<&Transform, &Earth>) {
+    gizmos
+        .circle(Vec3::ZERO, Vec3::Y, distance_to_sun, Color::RED)
+        .segments(128);
+
+    // moon orbit
+    let earth = query.single();
     gizmos
         .circle(
-            Vec3::ZERO,
+            earth.translation,
             Vec3::Y,
-            constant::Moon::DISTANCE_TO_EARTH * constant::SPACE_SCALE,
-            Color::RED,
+            constant::Moon::DISTANCE_TO_EARTH * SPACE_SCALE,
+            Color::DARK_GREEN,
         )
         .segments(128);
     gizmos.ray(
@@ -237,6 +282,5 @@ fn camera_control(
     }
     let mut camera = set.p0();
     let mut camera = camera.single_mut();
-    *camera.1 =
-        Transform::from_translation(target + (Vec3::splat(10.0))).looking_at(target, Vec3::Y);
+    *camera.1 = Transform::from_translation(target * 1.1).looking_at(target, Vec3::Y);
 }
